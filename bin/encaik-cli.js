d7c6d89d2f65457540ca2661fd83dd86f6bc66be
Encaik <843391622@qq.com>
2020-08-27 11:26:06 +0800

    添加命令行解析

--- bin/encaik-cli.js
+++ bin/encaik-cli.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
 let color = require("./color");
 let encaik = require("../src/index");
-let argvs = process.argv.slice(2);
-if (argvs.length === 0) {
+let optimist = require("optimist");
+let option = optimist
+  .usage("瞎几把使。\nUsage: encaik <commond> [option]")
+  .boolean("v")
+  .alias("v", "version")
+  .describe("v", "查看版本号")
+
+  .boolean("h")
+  .alias("h", "help")
+  .describe("h", "帮助信息");
+let argv = option.argv;
+let argvs = argv._;
+if (Object.keys(argv).length === 2 && argvs.length === 0) {
   encaik();
 }
-if (argvs.indexOf("-v") > -1 || argvs.indexOf("--version") > -1) {
+if (argv.v || argv.version) {
   console.log("version 1.0.0");
 }
-if (argvs.indexOf("-h") > -1 || argvs.indexOf("--help") > -1) {
-  console.log(
-    `
-    encaik                  run encaik
-    encaik -v/--version     get version of encaik
-    encaik -h/--help        help
-    `,
-  );
+if (argv.h || argv.help) {
+  optimist.showHelp((fn = console.log));
 }
 if (argvs.indexOf("-c") > -1 || argvs.indexOf("--color") > -1) {
   console.log(color("black", "white", "白底黑字"));

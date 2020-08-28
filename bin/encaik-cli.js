61640169d13d1a65c553e353936e06ae39f187f2
Encaik <843391622@qq.com>
2020-08-28 15:11:05 +0800

    小说和新闻功能

--- bin/encaik-cli.js
+++ bin/encaik-cli.js
@@ -8,6 +8,12 @@ let option = optimist
   .alias("v", "version")
   .describe("v", "查看版本号")
 
+  .boolean("news")
+  .describe("news", "获取新闻")
+
+  .string("book")
+  .describe("book", "获取小说")
+
   .boolean("c")
   .alias("c", "color")
   .describe("c", "查看色彩模式")
@@ -17,18 +23,19 @@ let option = optimist
   .describe("h", "帮助信息");
 let argv = option.argv;
 let argvs = argv._;
-if (argvs.length === 0) {
+if (argv.v || argv.version) {
+  console.log("version 1.0.0");
+} else if (argv.h || argv.help) {
+  optimist.showHelp((fn = console.log));
+} else if (argv.c || argv.color) {
+  console.log(color("black", "white", "白底黑字"));
+} else if (argv.news) {
+  encaik.getNews();
+} else if (argv.book) {
+  encaik.getBook(argv.book);
+} else if (argvs.length === 0) {
   console.log(color("black", "blue", "run"), "encaik");
   encaik();
   console.log(color("black", "red", "error"), "not found");
   console.log(color("black", "green", "OK"), "just try it!");
-}
-if (argv.v || argv.version) {
-  console.log("version 1.0.0");
-}
-if (argv.h || argv.help) {
-  optimist.showHelp((fn = console.log));
-}
-if (argv.c || argv.color) {
-  console.log(color("black", "white", "白底黑字"));
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,119 @@
-module.exports = function () {
-  console.log("index");
+const request = require("request");
+const cheerio = require("cheerio");
+const inquirer = require("inquirer");
+
+module.exports = {
+  getNews: function () {
+    let url = "https://www.tophub.fun:8888/GetRandomInfo";
+    request(url, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        let data = JSON.parse(body);
+        if (data.Code === 0) {
+          data.Data.forEach((i) => {
+            console.log(`[${i.type}]${i.Title}`);
+          });
+        } else {
+          console.log(data.Message);
+        }
+      } else {
+        console.log("网络请求失败！");
+      }
+    });
+  },
+  getBook: function (name) {
+    let form = {
+      searchkey: name,
+      searchtype: "all",
+    };
+    request.post(
+      {
+        url: "https://www.bxwxorg.com/search.html",
+        form,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+          let $ = cheerio.load(body);
+          let booknames = [];
+          let books = [];
+          $("#sitembox dl").each((index, item) => {
+            let a = $(item).find("h3 a");
+            booknames.push({
+              name: a.text(),
+              value: index,
+            });
+            books.push(a.attr("href"));
+          });
+          inquirer
+            .prompt([
+              {
+                name: "bookname",
+                type: "list",
+                message: "你想要看哪本书?",
+                choices: booknames,
+              },
+            ])
+            .then((ans) => {
+              request(
+                {
+                  url: books[ans.bookname],
+                  form,
+                  headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                  },
+                },
+                (err, res, body) => {
+                  if (!err && res.statusCode == 200) {
+                    let $ = cheerio.load(body);
+                    let nodenames = [];
+                    let nodes = [];
+                    $("#list dl dd").each((index, item) => {
+                      if (index < 12) {
+                        let a = $(item).find("a");
+                        nodenames.push({
+                          name: a.text(),
+                          value: index,
+                        });
+                        nodes.push(a.attr("href"));
+                      }
+                    });
+                    inquirer
+                      .prompt([
+                        {
+                          name: "nodename",
+                          type: "list",
+                          message: "你想要看哪个章节?",
+                          choices: nodenames,
+                        },
+                      ])
+                      .then((ans) => {
+                        request(
+                          {
+                            url: nodes[ans.nodename],
+                            form,
+                            headers: {
+                              "Content-Type":
+                                "application/x-www-form-urlencoded",
+                            },
+                          },
+                          (err, res, body) => {
+                            if (!err && res.statusCode == 200) {
+                              let $ = cheerio.load(body);
+                              console.log($("#content").text());
+                            }
+                          },
+                        );
+                      });
+                  }
+                },
+              );
+            });
+        } else {
+          console.error("请求出现失误，请重试！");
+        }
+      },
+    );
+  },
 };
